392792edd3468a49c693205b878bad386148eb1c
TyLinkous <tl987021@ohio.edu>
2023-01-27 14:50:51 -0500

    commit for the C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Ty Linkous
+ *        Date: 1/27/23
+ * Description: simple code with an output.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
